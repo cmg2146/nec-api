08b99e607e1c779ca448ab0ab1e97a7504e70704
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2023-02-25 23:14:49 -0800

    small fix: use settings object

--- src/app/dependencies.py
+++ src/app/dependencies.py
@@ -1,11 +1,10 @@
-import os
 from typing import Generator
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from app.settings import settings
 
-echo = os.getenv("FASTAPI_ENV") == "development"
+echo = settings.FASTAPI_ENV == "development"
 engine = create_async_engine(settings.DATABASE_URL, echo=echo)
 # async_sessionmaker: a factory for new AsyncSession objects.
 # expire_on_commit - don't expire objects after transaction commit
