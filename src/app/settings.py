ae11fbc318939c8053c99ed1ad4284b6d7fa48a1
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2023-03-02 11:12:54 -0800

    use preferred method for environment variable parsing

--- src/app/settings.py
+++ src/app/settings.py
@@ -1,4 +1,9 @@
-from pydantic import AnyHttpUrl, BaseSettings, DirectoryPath, PostgresDsn, validator
+from pydantic import (
+    BaseSettings,
+    AnyHttpUrl,
+    DirectoryPath,
+    PostgresDsn
+)
 
 class AppSettings(BaseSettings):
     FASTAPI_ENV: str
@@ -6,13 +11,11 @@ class AppSettings(BaseSettings):
     ALLOWED_ORIGINS: list[AnyHttpUrl] = []
     FILE_UPLOAD_DIR: DirectoryPath
 
-    @validator("ALLOWED_ORIGINS", pre=True)
-    def extract_allowed_origins(cls, v: str | list) -> list:
-        if isinstance(v, str):
-            return [origin.strip() for origin in v.split(",")]
-        elif isinstance(v, list):
-            return v
-        raise ValueError(v)
-
+    class Config:
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> any:
+            if field_name == 'ALLOWED_ORIGINS':
+                return [x.strip() for x in raw_val.split(",")]
+            return cls.json_loads(raw_val)
 
 settings = AppSettings()
